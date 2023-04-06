9d044bb76a1f8bad9069e0d3fb4f380475b81319
Habidhossen <habidhossen2@gmail.com>
2023-04-06 23:45:28 +0600

    index setup done

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./config/db_conn");
+const port = process.env.PORT || 4000;
+const app = express();
+
+// middleware
+app.use(
+  cors({ origin: "*", methods: ["GET", "POST", "PUT", "PATCH", "DELETE"] }) // for handling CORS policy error
+);
+app.use(express.json());
+
+// listening port
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
+
+// All Routes here...
+
+// Home Route
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to CipherSchools Server");
+});
+
+// If Route not found
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route Not Found!",
+  });
+});
+
+// if Server Error
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Something is broke!",
+  });
+});
