b7388e017af9c84995a180b5889cc472ad31928f
Habidhossen <habidhossen2@gmail.com>
2023-04-07 00:02:54 +0600

    user model added

---
+++ config/db_conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mjkw.mongodb.net/CipherSchools-Task?retryWrites=true&w=majority`()
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((err) => {
+    "Database Connection Failed: " + err;
+  });
